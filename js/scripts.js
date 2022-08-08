3bb2e5479a05684c5f9b4cdf534f090627bc3146
Ben <lawtonb16@gmail.com>
2022-08-08 16:40:29 -0500

    Changes For react move

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,14 @@ const backButton = document.querySelector('#backButton');
 
 
 
-const characters = [{name: "Antonio", weapon: "Whip", passive: "Gains 10% might per 10 levels(max 50%)"}, 
-                    {name: "Imelda", weapon: "Magic Wand", passive: "Gains 10% growth every 5 levels(max 30%)"},
-                    {name: "Pasqualina", weapon: "Runetracer", passive:"Gains 10% projectile speed every 5 levels(max 30%)" },
-                    {name: "Gennaro", weapon: "Knife", passive: "Grants +1 projectile(all weapons)"},
-                    {name: "Arca", weapon: "Fire Wand", passive: "Grants -5% cooldown every 10 levels(max -15%)"},
-                    {name: "Porta", weapon: "Lightning Ring", passive: "Grants +30% area, Grants -90% cooldown for the first three levels reducing by 30% every level until negated at level 4"}];
+const characters = [
+    {name: "Antonio", weapon: "Whip", passive: "Gains 10% might per 10 levels(max 50%)"}, 
+    {name: "Imelda", weapon: "Magic Wand", passive: "Gains 10% growth every 5 levels(max 30%)"},
+    {name: "Pasqualina", weapon: "Runetracer", passive:"Gains 10% projectile speed every 5 levels(max 30%)" },
+    {name: "Gennaro", weapon: "Knife", passive: "Grants +1 projectile(all weapons)"},
+    {name: "Arca", weapon: "Fire Wand", passive: "Grants -5% cooldown every 10 levels(max -15%)"},
+    {name: "Porta", weapon: "Lightning Ring", passive: "Grants +30% area, Grants -90% cooldown for the first three levels reducing by 30% every level until negated at level 4"}
+];
 
 
 antonioBtn.addEventListener("click", function () {loadCharacter(0)});
@@ -36,8 +38,6 @@ function loadCharacter(num) {
     passiveText.textContent = `Character Passive: ${characters[num].passive}`;
     cardBack.style.display = "block";
     backButton.style.display= "inline-block";
-
-    
 }
 
 
